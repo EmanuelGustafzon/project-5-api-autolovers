dc7e51aec79a088fdc4a5f927723e07ec65f8b86
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2022-09-12 18:35:52 +0000

    created first test for review app.

--- review/tests.py
+++ review/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Review
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class ReviewListViewTests(APITestCase):
+    def setUp(self):
+        User.objects.create_user(username='Emanuel', password='pass')
+
+    def test_can_list_reviews(self):
+        Emanuel = User.objects.get(username='Emanuel')
+        Review.objects.create(owner=Emanuel, model_year=1998)
+        response = self.client.get('/review/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(response.data)
+        print(len(response.data))
+
+ 
