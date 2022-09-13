fe0e83132ce0a56eab3d7cbf05f7f0de51de26b6
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2022-09-13 10:06:15 +0000

    Add annoted querysets to profle so users can see how many reviews a profile has, how many that profile is following and how many are follwing that profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,50 +1,32 @@
-from django.http import Http404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from django.db.models import Count
+from rest_framework import generics, filters
 from .models import Profile
 from .serializers import ProfileSerializer
 from carlovers.permissions import IsOwnerOrReadOnly
 
 
-class ProfileList(APIView):
-    """
-    List all profiles
-    No Create view (post method), as profile creation handled by django signals
-    """
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(
-            profiles, many=True, context={'request': request}
-            )
-        return Response(serializer.data)
-
-
-class ProfileDetail(APIView):
-    serializer_class = ProfileSerializer
-    permission_classes = [IsOwnerOrReadOnly]
-
-    def get_object(self, pk):
-        try:
-            profile = Profile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
+class ProfileList(generics.ListAPIView):
     
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(
-            profile, context={'request': request}
-            )
-        return Response(serializer.data)
+    queryset = Profile.objects.annotate(
+        reviews_count=Count('owner__review', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True),
+    ).order_by('-created_on')
+    serializer_class = ProfileSerializer
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_at',
+        'owner__followed__created_at',
+    ]
 
-    def put(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(
-            profile, data=request.data, context={'request': request}
-            )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
--- review/views.py
+++ review/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from rest_framework import status, permissions
+from rest_framework import status, permissions, generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Review
@@ -7,71 +7,18 @@ from .serializers import ReviewSerializer
 from carlovers.permissions import IsOwnerOrReadOnly
 
 
-class ReviewList(APIView):
-    """
-    List all profiles
-    No Create view (post method), as profile creation handled by django signals
-    """
+class ReviewList(generics.ListCreateAPIView):
+
     serializer_class = ReviewSerializer
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly
-    ]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Review.objects.all()
 
-    def get(self, request):
-        review = Review.objects.all()
-        serializer = ReviewSerializer(
-            review, many=True, context={'request': request}
-            )
-        return Response(serializer.data)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
-    def post(self, request):
-        serializer = ReviewSerializer(
-            data=request.data, context={'request': request}
-            )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.data, status=status.HTTP_400_BAD_REQUEST
-        )
-        
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
 
-
-class ReviewDetail(APIView):
-    serializer_class = ReviewSerializer
     permission_classes = [IsOwnerOrReadOnly]
-
-    def get_object(self, pk):
-        try:
-            review = Review.objects.get(pk=pk)
-            self.check_object_permissions(self.request, review)
-            return review
-        except review.DoesNotExist:
-            raise Http404
-    
-    def get(self, request, pk):
-        review = self.get_object(pk)
-        serializer = ReviewSerializer(
-            review, context={'request': request}
-            )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        review = self.get_object(pk)
-        serializer = ReviewSerializer(
-            review, data=request.data, context={'request': request}
-            )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        review = self.get_object(pk)
-        review.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+    serializer_class = ReviewSerializer
+    queryset = Review.objects.all()
