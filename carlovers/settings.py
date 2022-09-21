a4eb17778aee5796c47de89f632cb3553b4865a8
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2022-09-21 19:25:19 +0000

    Add extra cors headers fields in settings

--- carlovers/settings.py
+++ carlovers/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from corsheaders.defaults import default_headers, default_methods
 import re
 import dj_database_url
 
